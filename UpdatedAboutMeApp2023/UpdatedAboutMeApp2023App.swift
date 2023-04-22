0b14c81140dd028caf59f7f60726fab9d4e63c6b
Carolyn Hernandez <carolyh6@uci.edu>
2023-04-21 22:14:41 -0700

    Initial Commit

---
+++ UpdatedAboutMeApp2023/UpdatedAboutMeApp2023App.swift
@@ -0,0 +1,17 @@
+//
+//  UpdatedAboutMeApp2023App.swift
+//  UpdatedAboutMeApp2023
+//
+//  Created by Carolyn Hernandez on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct UpdatedAboutMeApp2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
